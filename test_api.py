456025a4f2947637e5a5a5e5a025debe28aec0ef
Ranjeet Pal <61289214+ranjeet012@users.noreply.github.com>
2026-06-10 17:29:49 +0530

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-API_KEY="fa0693990efe0eb39597a7155ee84143"
+API_KEY="Enter weather api key here"
 
 url=f"https://api.openweathermap.org/data/2.5/weather?q=Mumbai&appid={API_KEY}&units=metric"
 
